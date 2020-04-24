70bbedd3968974065e07d67219c69c6371156f7d
Moon Sung Hoon <moon9342@gmail.com>
2020-04-24 16:43:19 +0900

    commit

--- [4차산업혁명 인력양성사업-IoT과정]/src/javaArduino/Exam03_AndroidArduinoServer.java
+++ [4차산업혁명 인력양성사업-IoT과정]/src/javaArduino/Exam03_AndroidArduinoServer.java
@@ -1,9 +1,12 @@
 package javaArduino;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,6 +16,8 @@ import java.util.concurrent.Executors;
 import gnu.io.CommPort;
 import gnu.io.CommPortIdentifier;
 import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
@@ -23,12 +28,61 @@ import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 
+class ArduinoSerialListener implements SerialPortEventListener {
+
+//	BufferedReader br;
+	InputStream in;
+	BufferedWriter bw;
+	Exam03_AndroidArduinoServer window;
+	
+//	ArduinoSerialListener(BufferedReader br, BufferedWriter bw) {
+	ArduinoSerialListener(InputStream in, BufferedWriter bw, Exam03_AndroidArduinoServer window) {
+//		this.br = br;
+		this.in = in;
+		this.bw = bw;
+		this.window = window;
+	}
+	
+	@Override
+	public void serialEvent(SerialPortEvent oEvent) {
+		if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+            try {
+            	
+            	int available = in.available();
+            	System.out.println("받은 바이트수 : " + available);
+                byte chunk[] = new byte[available];
+                in.read(chunk, 0, available);
+
+            	// 아두이노로부터 ACK메시지 받기
+//                 String fromArduino = br.readLine();
+            	
+                // 바로 출력                
+//                 if( (fromArduino != null) && (fromArduino.length() != 0) ) {
+//                	 System.out.println("아두이노에서 받은 메시지 : " + fromArduino);	 
+//                 }
+                 
+                System.out.println("아두이노에서 받은 메시지 : " + new String(chunk));
+                
+                window.pr.println(new String(chunk));
+                window.pr.flush();
+                
+            } catch (Exception e) {
+                System.err.println(e.toString());
+            }
+        }		
+		
+	}	
+}
+
 public class Exam03_AndroidArduinoServer extends Application {
 
 	TextArea textarea;
 	Button btn;
 	ServerSocket server;
 	ExecutorService executorService = Executors.newCachedThreadPool();
+	BufferedReader br;
+	BufferedWriter bw;
+	PrintWriter pr;
 	
 	private void printMessage(String msg) {
 		Platform.runLater(()->{
@@ -56,13 +110,20 @@ public class Exam03_AndroidArduinoServer extends Application {
 					Socket s = server.accept();
 					printMessage("[Android Arduino Server 기동-클라이언트 접속 성공]");
 					BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-					PrintWriter pr = new PrintWriter(s.getOutputStream());
+					pr = new PrintWriter(s.getOutputStream());
 					String line = "";
 					while(true) {
 						if((line = br.readLine()) != null) {
 							if(line.equals("LED_ON")) {
 								printMessage("불이켜져요!!");
+								bw.write(line,0,line.length());								
+								bw.flush();
 							}
+							if(line.equals("LED_OFF")) {
+								printMessage("불이꺼져요!!");
+								bw.write(line,0,line.length());								
+								bw.flush();
+							}							
 						}
 					}
 				} catch (IOException e1) {
@@ -86,9 +147,46 @@ public class Exam03_AndroidArduinoServer extends Application {
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Android Arduino Server");
 		primaryStage.setOnCloseRequest(e->{
-			
+			System.exit(0);
 		});
 		primaryStage.show();
+		
+        CommPortIdentifier portIdentifier = null;		
+		try {
+			portIdentifier = CommPortIdentifier.getPortIdentifier("COM14");
+			
+	        if ( portIdentifier.isCurrentlyOwned() ) {
+	        	System.out.println("현재 포트가 사용되고 있습니다.");
+	        } else {
+
+	          CommPort commPort = 
+	    		portIdentifier.open("TT",2000);
+	        	
+	            if ( commPort instanceof SerialPort ) {
+	                //포트 설정(통신속도 설정. 기본 9600으로 사용)
+	                SerialPort serialPort = (SerialPort) commPort;
+						serialPort.setSerialPortParams(9600,
+								SerialPort.DATABITS_8,
+								SerialPort.STOPBITS_1,
+								SerialPort.PARITY_NONE);
+
+					//Input,OutputStream 버퍼 생성 후 오픈
+					InputStream in = serialPort.getInputStream();
+					br = new BufferedReader(new InputStreamReader(in));	
+		            bw = new BufferedWriter(new OutputStreamWriter(serialPort.getOutputStream()));
+		                
+//	                serialPort.addEventListener(new ArduinoSerialListener(br,bw));
+		            serialPort.addEventListener(new ArduinoSerialListener(in,bw,this));
+	                serialPort.notifyOnDataAvailable(true);
+	                                
+	            } else {
+	            	System.out.println("Serial 포트만 사용가능합니다.");
+	            }
+	        }     
+			
+		} catch(Exception e) {
+			System.out.println(e);
+		}
 		
 	}
 
